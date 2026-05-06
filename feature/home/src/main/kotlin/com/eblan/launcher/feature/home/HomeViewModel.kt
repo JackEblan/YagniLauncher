4b90f56ed3bde75d65347a3a5ce4b049deffb7bf
Jack Eblan <jackeblan115@gmail.com>
2026-05-06 11:13:44 +0800

    Cleaned up unused `GetFolderGridItemsUseCase` and redundant logic in `HomeViewModel`:
    - Removed the `GetFolderGridItemsUseCase` dependency from `HomeViewModel`.
    - Deleted redundant calls to `getFolderGridItemsUseCase()` within drag-and-drop handling logic that were not being utilized or assigned to state.
    - Removed an unused grid item aggregation line during the grid update process.

--- feature/home/src/main/kotlin/com/eblan/launcher/feature/home/HomeViewModel.kt
+++ feature/home/src/main/kotlin/com/eblan/launcher/feature/home/HomeViewModel.kt
@@ -46,7 +46,6 @@ import com.eblan.launcher.domain.usecase.application.GetEblanShortcutConfigsByLa
 import com.eblan.launcher.domain.usecase.application.GetEblanShortcutInfosUseCase
 import com.eblan.launcher.domain.usecase.application.UpdateEblanApplicationInfosIndexesUseCase
 import com.eblan.launcher.domain.usecase.grid.GetFolderGridItemsByIdUseCase
-import com.eblan.launcher.domain.usecase.grid.GetFolderGridItemsUseCase
 import com.eblan.launcher.domain.usecase.grid.MoveFolderGridItemUseCase
 import com.eblan.launcher.domain.usecase.grid.MoveGridItemUseCase
 import com.eblan.launcher.domain.usecase.grid.ResizeGridItemUseCase
@@ -107,7 +106,6 @@ internal class HomeViewModel @Inject constructor(
     getFolderGridItemsByIdUseCase: GetFolderGridItemsByIdUseCase,
     private val moveFolderGridItemUseCase: MoveFolderGridItemUseCase,
     private val iconKeyGenerator: IconKeyGenerator,
-    private val getFolderGridItemsUseCase: GetFolderGridItemsUseCase,
 ) : ViewModel() {
     val homeUiState = getHomeDataUseCase().map(HomeUiState::Success).stateIn(
         scope = viewModelScope,
@@ -356,8 +354,6 @@ internal class HomeViewModel @Inject constructor(
 
             updateGridItemsAfterMoveUseCase(moveGridItemResult = moveGridItemResult)
 
-            gridRepository.getGridItems().plus(getFolderGridItemsUseCase())
-
             _isVisibleOverlay.update {
                 false
             }
@@ -602,8 +598,6 @@ internal class HomeViewModel @Inject constructor(
     fun resetGridAfterMoveFolder() {
         viewModelScope.launch {
             moveGridItemJob?.cancelAndJoin()
-
-            getFolderGridItemsUseCase()
 
             _isVisibleOverlay.update {
                 false
