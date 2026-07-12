2577a867e91da5271fc04d8c85d03b439414b8db
Jack Eblan <jackeblan115@gmail.com>
2026-07-13 07:22:05 +0800

    fix: Update version code and name (84)
    Updates the version code to 84 and the version name to "0.8.4-alpha" in `app/build.gradle.kts`.
    Also adds a new changelog entry for version 84 in `fastlane/metadata/android/en-US/changelogs/84.txt`.
    This commit addresses a version bump and release preparation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
 
     defaultConfig {
         applicationId = "com.eblan.launcher"
-        versionCode = 83
-        versionName = "0.8.3-alpha"
+        versionCode = 84
+        versionName = "0.8.4-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
